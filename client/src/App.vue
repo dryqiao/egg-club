cf37fdcdad64d790483b46080695be826c1e9478
qiao <445271466@qq.com>
2018-01-21 23:31:11 +0800

    header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <v-header></v-header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import header from './components/header'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    'v-header' : header
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
 </style>
---
+++ client/src/components/header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <a href="/"></a>
+        <ul class="nav">
+            <li v-for="item in list">{{item.name}}</li>
+        </ul>
+    </header>    
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                list:[{
+                    name: '关于',
+                },{
+                    name: '设置',
+                }]
+            }
+        }
+    }
+</script>
+<style lang="less" scoped>
+header {
+    padding: 5px 10px 5px 10px;
+    height: 60px;
+    background: #444;
+    color: #ccc;
+    .nav {
+        display: flex;
+        li {
+            margin-left: 10px;
+        }
+    }
+}
+</style>
